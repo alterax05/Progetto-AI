aaef96246a761fe3b6a000a43491e130993a352a
alterax05 <miky.porcellato@gmail.com>
2024-01-14 18:19:41 +0100

    First Implementation of ONNX model with MNIST dataset

--- frontend-react/src/pages/Playground.tsx
+++ frontend-react/src/pages/Playground.tsx
@@ -1,45 +1,96 @@
 import { InferenceSession, Tensor } from "onnxruntime-web";
 
-import {Button} from "@/components/ui/button.tsx";
+import { Button } from "@/components/ui/button.tsx";
 
-import React, { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState, MouseEvent } from "react";
+
+import pica from "pica";
 
 function Playground() {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
   const contextRef = useRef<CanvasRenderingContext2D | null>(null);
-  const [isDrawing, setIsDrawing] = React.useState(false);
-  const inferenceSession =  async () => { return await InferenceSession.create("@/model.onnx")};
+
+  const [isDrawing, setIsDrawing] = useState(false);
+  const [predictedClass, setPredictedClass] = useState<number | null>(null);
 
   useEffect(() => {
     const canvas = canvasRef.current;
     if (canvas) {
       canvas.width = 500;
       canvas.height = 500;
-      //canvas.style.width = `${window.innerWidth}px`;
-      //canvas.style.height = `${window.innerHeight}px`;
+      canvas.style.width = "500px";
+      canvas.style.height = "500px";
 
-      const context = canvas.getContext('2d');
+      const context = canvas.getContext("2d", { willReadFrequently: true });
       if (context) {
         context.scale(1, 1);
-        context.lineCap = 'round';
-        context.strokeStyle = 'black';
-        context.lineWidth = 5;
+        context.lineCap = "round";
+        context.strokeStyle = "black";
+        context.lineWidth = 20;
         contextRef.current = context;
       }
     }
   }, []);
 
-  const updatePredictions = async () => {
-    if (contextRef.current && canvasRef.current) {
-      const image  = contextRef.current.getImageData(0, 0, canvasRef.current.width , canvasRef.current.height).data;
-      const input = new Tensor("float32", new Float32Array(image), [1, 1, 28, 28]);
-      const session = await inferenceSession();
-      const predictions = await session.run({ input });
-      console.log(predictions);
-    }
+  const inferenceSession = async () => {
+    return await InferenceSession.create("/model.onnx", {
+      executionProviders: ["webgl"],
+      graphOptimizationLevel: "all",
+    });
   };
 
-  const startDrawing = ({ nativeEvent }: React.MouseEvent<HTMLCanvasElement>) => {
+  function imageDataToTensor(
+    imageBuffer: Uint8ClampedArray,
+    dims: number[]
+  ): Tensor {
+    //Get only the alpha channel
+    const alphaChannel = new Array<number>();
+    for (let i = 0; i < imageBuffer.length; i += 4) {
+      alphaChannel.push(imageBuffer[i + 3]);
+    }
+    // 2. transpose the image data from [28, 28, 1]
+    const float32Data = new Float32Array(dims[1] * dims[2] * dims[3]);
+    for (let i = 0; i < alphaChannel.length; i++) {
+      float32Data[i] = alphaChannel[i] / 255.0;
+    }
+    // 3. create the tensor object from onnxruntime-web.
+    const inputTensor = new Tensor("float32", float32Data, dims);
+    return inputTensor;
+  }
+
+  const updatePredictions = async () => {
+    if (contextRef.current && canvasRef.current) {
+      const image = contextRef.current.getImageData(
+        0,
+        0,
+        canvasRef.current.width,
+        canvasRef.current.height
+      );
+      if (!image) {
+        return;
+      }
+      const resizeImageData = new Uint8ClampedArray(
+        await pica().resizeBuffer({
+          src: new Uint8Array(image.data.buffer),
+          width: image.width,
+          height: image.height,
+          toWidth: 28,
+          toHeight: 28,
+        })
+      );
+
+      const input = imageDataToTensor(resizeImageData, [1, 1, 28, 28]);
+      const session = await inferenceSession();
+      const feeds: Record<string, Tensor> = {};
+      feeds[session.inputNames[0]] = input;
+      const outputMap = await session.run(feeds);
+      const outputTensor = outputMap[session.outputNames[0]];
+      const outputData = outputTensor.data as Float32Array;
+      const predictedClass = outputData.indexOf(Math.max(...outputData));
+      setPredictedClass(predictedClass);
+    }
+  };
+  const startDrawing = ({ nativeEvent }: MouseEvent<HTMLCanvasElement>) => {
     const { offsetX, offsetY } = nativeEvent;
     if (contextRef.current) {
       contextRef.current.beginPath();
@@ -53,9 +104,10 @@ function Playground() {
       contextRef.current.closePath();
     }
     setIsDrawing(false);
+    updatePredictions();
   };
 
-  const draw = ({ nativeEvent }: React.MouseEvent<HTMLCanvasElement>) => {
+  const draw = ({ nativeEvent }: MouseEvent<HTMLCanvasElement>) => {
     if (!isDrawing) {
       return;
     }
@@ -66,7 +118,6 @@ function Playground() {
     if (contextRef.current) {
       contextRef.current.stroke();
     }
-    updatePredictions();
   };
 
   const clearCanvas = () => {
@@ -75,23 +126,25 @@ function Playground() {
     if (context && canvas) {
       context.clearRect(0, 0, canvas.width, canvas.height);
     }
+    setPredictedClass(null);
   };
 
   return (
     <div>
-    <h1 className="text-5xl m-16">
+      <h1 className="text-5xl m-16">
         <b>Playground</b>
       </h1>
-    <canvas
-      onMouseDown={startDrawing}
-      onMouseUp={finishDrawing}
-      onMouseMove={draw}
-      ref={canvasRef}
-      className="border-2 border-black rounded-md"
-    />
-    <Button className="" onClick={clearCanvas}>
-      <b>Elimina</b>
-    </Button>
+      <p>{predictedClass}</p>
+      <canvas
+        onMouseDown={startDrawing}
+        onMouseUp={finishDrawing}
+        onMouseMove={draw}
+        ref={canvasRef}
+        className="border-2 border-black rounded-md"
+      />
+      <Button onClick={clearCanvas}>
+        <b>Elimina</b>
+      </Button>
     </div>
   );
 }
--- modello-ai/train-model.py
+++ modello-ai/train-model.py
@@ -54,6 +54,7 @@ def evaluate(model):
 
 
 def toONNX(model, filename):
+    #                        (batch, channel, width and height)
     dummy_input = torch.randn(1, 1, 28, 28).to(device)
     torch.onnx.export(model, dummy_input, filename, verbose=True)
 
