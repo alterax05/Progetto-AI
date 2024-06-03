d19c1600c62fd9a74ec09fc483ee7442c5417acc
alterax05 <107863063+alterax05@users.noreply.github.com>
2024-06-03 22:49:28 +0200

    exctact theme picker in its own component

--- frontend-react/src/pages/Home.tsx
+++ frontend-react/src/pages/Home.tsx
@@ -1,7 +1,6 @@
 import { buttonVariants } from "@/components/ui/button";
 import {
   Card,
-  CardContent,
   CardDescription,
   CardFooter,
   CardHeader,
@@ -10,104 +9,75 @@ import {
 import { MoveRight } from "lucide-react";
 import { Separator } from "@/components/ui/separator";
 import { Link } from "react-router-dom";
-import { Moon, Sun } from "lucide-react";
-import "./App.css";
-
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { useTheme } from "@/components/theme-provider";
-import { useTypewriter, Cursor } from "react-simple-typewriter";
+import ThemePicker from "@/components/ThemePicker";
+import { H1 } from "@/components/typografy/h1";
 
 function Home() {
-  const { setTheme } = useTheme();
-  const [text] = useTypewriter({
-    words: ["Progetto", "Intelligenza", "Artificiale"],
-    loop: false,
-    typeSpeed: 120,
-    delaySpeed:80,
-  });
-  
   return (
-    <div className="anim_gradient">
-      <div className="absolute right-0 p-4">
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="outline" size="icon">
-              <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-              <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 dark:text-white" />
-              <span className="sr-only">Toggle theme</span>
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end">
-            <DropdownMenuItem onClick={() => setTheme("light")}>
-              Light
-            </DropdownMenuItem>
-            <DropdownMenuItem onClick={() => setTheme("dark")}>
-              Dark
-            </DropdownMenuItem>
-            <DropdownMenuItem onClick={() => setTheme("system")}>
-              System
-            </DropdownMenuItem>
-          </DropdownMenuContent>
-        </DropdownMenu>
+    <div className="anim_gradient h-screen flex flex-col justify-center items-center">
+      <div className="absolute top-4 right-4">
+        <ThemePicker />
       </div>
-      <div className="flex flex-col h-screen items-center justify-center">
-        <div className="flex flex-row items-center justify-self-center h-screen">
-          <h1 className="text-5xl m-16 fixed left-20">
-            <span style={{ fontWeight: "bold", color: "white" }}>{text}</span>
-            <span style={{ color: "#EFE7DA" }}>
-              <Cursor cursorStyle="<" />
-            </span>
-          </h1>
-          <div className="flex items-center space-x-10 fixed right-45">
-            <Separator orientation="vertical" className="h-[50vh] w-0.5 absolute"/>
-            <Card className="hover:drop-shadow-lg">
-              <CardHeader>
-                <CardTitle>Ricerca TPSI</CardTitle>
-                <CardDescription>
-                  Clicca qui per visualizzare la ricerca
-                </CardDescription>
-              </CardHeader>
-              <CardContent className="grid gap-4"></CardContent>
-              <CardFooter className="flex justify-end">
-                <Link
-                  to="/tpsi"
-                  className={buttonVariants({
-                    variant: "default",
-                    size: "icon",
-                  })}
-                >
-                  <MoveRight />
-                </Link>
-              </CardFooter>
-            </Card>
-            <Card className="hover:drop-shadow-lg">
-              <CardHeader>
-                <CardTitle>Playground AI</CardTitle>
-                <CardDescription>
-                  Clicca qui per provare l'intelligenza artificiale
-                </CardDescription>
-              </CardHeader>
-              <CardContent className="grid gap-4"></CardContent>
-              <CardFooter className="flex justify-end">
-                <Link
-                  to="/playground"
-                  className={buttonVariants({
-                    variant: "default",
-                    size: "icon",
-                  })}
-                >
-                  <MoveRight />
-                </Link>
-              </CardFooter>
-            </Card>
-          </div>
-        </div>
+      <div className="flex flex-col lg:flex-row items-center justify-center space-y-10 lg:space-y-0 lg:space-x-10">
+        <H1 className="text-white">Progetto AI</H1>
+        <Separator
+          orientation="vertical"
+          className="hidden lg:block h-[50vh] w-0.5"
+        />
+        <Card className="hover:drop-shadow-lg lg:min-h-[20vh] max-h-[30vh] w-[30vh]">
+          <CardHeader>
+            <CardTitle>Ricerca TPSI</CardTitle>
+            <CardDescription>
+              Clicca qui per visualizzare la ricerca
+            </CardDescription>
+          </CardHeader>
+          <CardFooter>
+            <Link
+              to="/tpsi"
+              className={buttonVariants({
+                variant: "default",
+                size: "icon",
+              })}
+            >
+              <MoveRight />
+            </Link>
+          </CardFooter>
+        </Card>
+        <Card className="hover:drop-shadow-lg lg:min-h-[20vh] max-h-[30vh] w-[30vh]">
+          <CardHeader>
+            <CardTitle>Playground AI</CardTitle>
+            <CardDescription>
+              Clicca qui per provare l'intelligenza artificiale
+            </CardDescription>
+          </CardHeader>
+          <CardFooter>
+            <Link
+              to="/playground"
+              className={buttonVariants({
+                variant: "default",
+                size: "icon",
+              })}
+            >
+              <MoveRight />
+            </Link>
+          </CardFooter>
+        </Card>
+        <Card className="hover:drop-shadow-lg lg:min-h-[20vh] max-h-[30vh] w-[30vh]">
+          <CardHeader>
+            <CardTitle>ChatGame</CardTitle>
+            <CardDescription>
+              Clicca qui per provare un altro esempio di AI
+            </CardDescription>
+          </CardHeader>
+          <CardFooter>
+            <a href="https://chatgame.5cimicheleporcellato.barsanti.edu.it" className={buttonVariants({
+                variant: "default",
+                size: "icon",
+              })}>
+              <MoveRight />
+            </a>
+          </CardFooter>
+        </Card>
       </div>
     </div>
   );
