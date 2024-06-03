56f964081fca5dc526819420cdf596675e5fdbd5
alterax05 <107863063+alterax05@users.noreply.github.com>
2024-06-03 22:49:41 +0200

    updated error page

--- frontend-react/src/pages/ErrorPage.tsx
+++ frontend-react/src/pages/ErrorPage.tsx
@@ -1,16 +1,10 @@
-import { useRouteError } from "react-router-dom";
-
+import { H1 } from "@/components/typografy/h1";
+import { P } from "@/components/typografy/p";
 function ErrorPage() {
-  const error: any = useRouteError();
-  console.error(error);
-
   return (
-    <div id="error-page">
-      <h1><b>Oops!</b></h1>
-      <p>Scusa, è accaduto un errore inaspettato.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
+    <div id="error-page" className="anim_gradient flex items-center justify-center min-h-screen flex-col">
+      <H1 className="text-white"><b>Oops!</b></H1>
+      <P className="text-white">Scusa, ma la pagina che hai cercato non esiste ಥ_ಥ</P>
     </div>
   );
 }
